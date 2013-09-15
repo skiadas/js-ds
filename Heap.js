0c5470cbc996d2b4b6c1ca65b8fc02a146c4426d
Haris Skiadas <cskiadas@gmail.com>
2013-09-15 16:09:26 -0400

    Improved Heap implementation

--- Heap.js
+++ Heap.js
@@ -10,40 +10,48 @@ define(function(require) {
     var floor = Math.floor,
         swap = function(i, j) {
         // Make sure to call it with the correct "this" binding
-            var a = this.get(i),
-                b = this.get(j);
-            this.set(i, b);
-            this.set(j, a);
+            var items = this.items,
+                temp = items[i];
+            items[i] = items[j];
+            items[j] = temp;
+            items[i].index = i;
+            items[j].index = j;
         },
-        fix = function(obj) {
-        // Attempts to fix situation rooted at obj. Moves up or down accordingly
-        var i = this.getIndex(obj),
-            left = 2*i, right = 2*i + 1,
-            lItem = this.get(left),
-            rItem = this.get(right),
-            curr = this.get(i),
-            par = floor(i/2),
-            isLess = this.isLess;
-        // console.log(lItem, rItem, curr, par, par >= 1 && this.get(par));
-        if (lItem && rItem && isLess(rItem, lItem) && isLess(rItem, curr)) {
-                swap.call(this, i, right);
-        } else if (lItem && isLess(lItem, curr)) {
-            swap.call(this, i, left);
-        } else if (par >= 1 && isLess(curr, this.get(par))) {
-            swap.call(this, i, par);
-        } else { // Nothing to fix
+        sink = function(k) {
+        // Fixes down from k.
+            var j = 2 * k,
+                l = this.length;
+            while(j <= l) {
+                if (j < l && this.comp(j + 1, j)) { j++; }
+                if (this.comp(k, j)) { break; }
+                swap.call(this, k, j);
+                k = j;
+                j = 2 * k;
+            }
             return this;
-        }
-        return fix.call(this, obj);
-    }
+        },
+        swim = function(k) {
+        // Fixes up from k.
+            var p = floor(k/2);
+            while(p > 0) {
+                // console.log(this, k, p);
+                if (this.comp(p, k)) { break; }
+                swap.call(this, p, k);
+                k = p;
+                p = floor(k/2);
+            }
+            return this;
+        },
+        noop = function() { };
 
-    function Heap(isLess) {
+    function Heap(isLess, canDelete) {
         // isLess needs to be either a function f(a,b) or an object property 
         // name. It is used to determine when an object a is "less" than an
         // object b for the purposes of the heap
         // Our arrays have an extra empty value in the front, to work
         // better with the numbering that standard heap method uses.
         this.items = [null];
+        this.canDelete = canDelete || false;
         // Length keeps the number of items in the heap (so 1 off from array)
         this.length = 0;
         this.isLess = (typeof isLess === 'function') ?
@@ -52,66 +60,60 @@ define(function(require) {
         // The following section provides unique IDs to keep track of object 
         // location in heap. Key used to decorate objects with their location
         // in the heap;
-        var randomKey =
-            String.fromCharCode(32 + Math.floor(Math.random() * 90 )) +
-            Math.random();
-        this.init = function(obj, index) {
-            if (obj[randomKey]) { console.log("oops")}
-            Object.defineProperty(obj, randomKey, {
-                configurable: true,
-                writable: true,
-                value: index
-            });
+        if (canDelete) {
+            var randomKey =
+                String.fromCharCode(32 + Math.floor(Math.random() * 90 )) +
+                Math.random();
+            this.init = function(item) {
+                var newObj = {value: item, index: ++this.length };
+                Object.defineProperty(item, randomKey, {
+                    configurable: true, value: newObj
+                });
+                return newObj;
+            };
+            this.getIndex = function(obj) { return obj[randomKey].index; };
+            this.removeIndex = function(obj) { delete obj[randomKey]; };
         }
-        this.setIndex = function(obj, index) {
-            obj[randomKey] = index;
-        }
-        this.getIndex = function(obj) { return obj[randomKey]; }
-        this.removeIndex = function(obj) { delete obj[randomKey]; }
     }
 
     Heap.prototype = {
-        get: function(i) {
-            return (i != null) ? this.items[i] : this.items.slice(1);
-        },
-        set: function(i, val) {
-            this.items[i] = val;
-            if (i === this.length + 1) {
-                this.init(val, i);
-                this.length++;
-                fix.call(this, val); 
-            } else {
-                this.setIndex(val, i);
+        init: function(obj) { return {value: obj, index: ++this.length }; },
+        removeIndex: noop,
+        comp: function(i, j) { return this.isLess(this.items[i].value, this.items[j].value); },
+        get: function(i) { return this.items[i].value; },
+        index: function(obj) {
+            if (this.canDelete) {
+                return this.getIndex(obj) || null;
             }
-            return this;
+            for (var i = 1; i < this.length + 1; i++) {
+                if (this[i].value === obj) { return i; }
+            }
+            return null;
         },
-        has: function(obj) {
-            return (this.getIndex(obj) != null);
-        },
-        pop: function() {
-            if (this.length == 0) throw new Error("Cannot remove from empty list.");
-            this.removeIndex(this.items[this.length--]);
-            return this.items.pop();
-        },
-        delete: function(obj) {
+        deleteObj: function(obj) {
             // Deletes the object, by swapping it with the last entry.
             // Then either bubbles up or down as needed. Returns the deleted element.
+            if (!this.canDelete) throw new Error("Cannot delete from this type of Heap.");
             var i = this.getIndex(obj);
-            if (!(i > 0 && i <= this.length)) { return; }
+            return (i == null) ? null : this.delete(i);
+        },
+        delete: function(i) {
+            var obj = this.get(i);
+            if (this.length == 0) throw new Error("Cannot remove from empty list.");
             swap.call(this, i, this.length);
-            this.pop();
-            if (i - 1 !== this.length && this.length > 0) fix.call(this, this.get(i));
+            this.removeIndex(this.items[this.length--]);
+            this.items.pop();
+            sink.call(this, i);
             return obj;
         },
-        leadvalue: function() { return this.get(1); },
-        extract: function() { return this.delete(this.get(1)); },
+        min: function() { return this.get(1); },
+        extract: function() { return this.delete(1); },
         insert: function(newItem) {
-            this.set(this.length + 1, newItem);
+            this.items.push(this.init(newItem));
+            swim.call(this, this.length);
             return this;
         },
-        isEmpty: function() {
-            return this.length === 0;
-        }
+        isEmpty: function() { return this.length === 0; }
     }
     
     return Heap;
