e60a8110cd692dfb2adbea1d582c90b965b07cf9
Haris Skiadas <cskiadas@gmail.com>
2013-09-27 00:25:19 -0400

    Improved RBTs

--- RBT.js
+++ RBT.js
@@ -16,8 +16,13 @@ define(function(require) {
     
     //Inherit all properties from BST Null
     utils.extend(Null, BST.Node.Null, {
+        left: Null,
+        right: Null,
         isRed: function() { return false; },
-        put: function(key, value) { return new Node(key ,value); }
+        isBlack: function() { return true; },
+        balance: function() { return this; },
+        put: function(key, value) { return new Node(key ,value); },
+        isRBT: function() { return 1; } // Returns "black count"
     });
     // Use that instead of an explicit color setting to ensure it does not change
     // Ensure we're using the RBT Node version
@@ -30,10 +35,12 @@ define(function(require) {
             color: RED
         })
     }
-    
+
+    Node.Null = Null;
     Node.prototype = new BST.Node();
     utils.extend(Node.prototype, {
         isRed: function() { return this.color === RED; },
+        isBlack: function() { return !this.isRed(); },
         _rotateLeft: BST.Node.prototype.rotateLeft,
         rotateLeft: function() {
             var that = this._rotateLeft();
@@ -51,10 +58,8 @@ define(function(require) {
             return that;
         },
         flipColors: function() {
-            if ((this.left.isRed()) && (this.right.isRed())) {
-                this.left.color = this.right.color = BLACK;
-                this.color = RED;
-            }
+            this.left.color = this.right.color = (this.color === RED ? RED : BLACK);
+            this.color = (this.color === RED ? BLACK : RED);
             return this;
         },
         put: function(key, value, compare) {
@@ -65,9 +70,77 @@ define(function(require) {
                 that[side] =  that[side].put(key, value, compare);
             }
             that.fixSize();
-            if (that.right.isRed() && !that.left.isRed()) { that = that.rotateLeft(); }
+            if (that.right.isRed() && that.left.isBlack()) { that = that.rotateLeft(); }
             if (that.left.isRed() && that.left.left.isRed()) { that = that.rotateRight(); }
-            return that.flipColors();
+            if (that.left.isRed() && that.right.isRed()) that.flipColors();
+            return that;
+        },
+        balance: function() {
+            return (this.right.isRed() ? this.rotateLeft() : this);
+        },
+        moveRedLeft: function() {
+            this.flipColors();
+            var that = this;
+            if (that.right.left.isRed()) {
+                that.right = that.right.rotateRight();
+                that = that.rotateLeft();
+            }
+            return that;
+        },
+        moveRedRight: function() {
+            // var that = this;
+            this.flipColors();
+            var that = this;
+            return (that.left.left.isRed() ? that : that.rotateRight());
+        },
+        deleteMin: function() {
+            if (this.left.isNull()) return this.right;
+            var that = this;
+            if (that.left.isBlack() && that.left.left.isBlack()) {
+                that = that.moveRedLeft();
+            }
+            that.left = that.left.deleteMin();
+            return that.fixSize().balance();
+        },
+        deleteMax: function() {
+            var that = this;
+            if (that.left.isRed()) that = that.rotateRight();
+            if (that.right.isNull()) return that.left;
+            if (that.right.isBlack() && that.right.left.isBlack()) that = that.moveRedRight();
+            that.right = that.right.deleteMax();
+            return that.fixSize().balance();
+        },
+        delete: function(key, compare) {
+            var that = this,
+                comp = compare(key, this.key);
+            if (comp < 0) {
+                if (that.left.isBlack() && that.left.left.isBlack()) {
+                    that = that.moveRedLeft();
+                }
+                that.left = that.left.delete(key, compare);
+                return that.fixSize().balance();
+            }
+            if (that.left.isRed()) that = that.rotateRight();
+            if (compare(key, that.key) > 0) {
+                that.right = that.right.delete(key, compare);
+                return that.fixSize().balance();
+            } 
+            // Found the key
+            if (that.right.isNull()) return that.left.fixSize().balance();
+            var x = that.right.min();
+            that.key = x.key;
+            that.value = x.value;
+            that.right = that.right.deleteMin();
+            return that.fixSize().balance();
+        },
+        isRBT: function() {
+            // Returns "null" if it is not RBT, or the "black count" otherwise
+            if (this.right.isRed()) return false;
+            if (this.isRed() && this.left.isRed()) return false;
+            var bcl = this.left.isRBT(),
+                bcr = this.right.isRBT();
+            if (bcl === null || bcr === null || bcl !== bcr) return false;
+            return bcl + (this.isRed() ? 0 : 1);
         }
     });
     
@@ -76,10 +149,33 @@ define(function(require) {
         this.root = Null;
     }
 
+    RBT.Node = Node;
     RBT.prototype = new BST();
     utils.extend(RBT.prototype, {
         put: function(key, value) {
             this.root = this.root.put(key, value, this.compare);
+            this.root.color = BLACK;
+            return this;
+        },
+        isRBT: function() {
+            return (this.root.isBlack() && this.root.isRBT() !== null);
+        },
+        deleteMin: function() {
+            if (this.root.left.isBlack() && this.root.right.isBlack()) this.root.color = RED;
+            this.root = this.root.deleteMin();
+            this.root.color = BLACK;
+            return this;
+        },
+        deleteMax: function() {
+            if (this.root.left.isBlack() && this.root.right.isBlack()) this.root.color = RED;
+            this.root = this.root.deleteMax();
+            this.root.color = BLACK;
+            return this;
+        },
+        delete: function(key) {
+            if (!this.has(key)) return this;
+            if (this.root.left.isBlack() && this.root.right.isBlack()) this.root.color = RED;
+            this.root = this.root.delete(key, this.compare);
             this.root.color = BLACK;
             return this;
         }
--- tests/RBTTest.js
+++ tests/RBTTest.js
@@ -19,17 +19,83 @@ describe("Red Black Trees", function() {
         expect(t.get(key)).to.equal(val);
         expect(t.get(key + 1) == null).to.be.true;
         expect(t.isBST()).to.be.true;
+        expect(t.isRBT()).to.be.true;
     });
-    it("increase size with each new element", function() {
+    it("increase size with each new element and maintain the red-black properties", function() {
        var N = 100;
        for (var i = 1; i < N; i++) {
            t.put(Math.random(), "foo");
            expect(t.size()).to.equal(i);
            expect(t.isBST()).to.be.true;
+           expect(t.isRBT()).to.be.true;
        }
     });
     it("assign a color to each node", function() {
         t.put(23);
-        expect(t.root.isRed()).to.equal.false;
+        expect(t.root.key).to.equal(23);
+        expect(t.root.isRed()).to.be.false;
+        expect(t.root.isRBT()).to.equal(2);
+        t.put(34);
+        expect(t.root.key).to.equal(34);
+        expect(t.root.isRed()).to.be.false;
+        expect(t.root.left.key).to.equal(23);
+        expect(t.root.left.isRed()).to.be.true;
+        expect(t.root.isRBT()).to.equal(2);
+        t.put(45);
+        expect(t.root.key).to.equal(34);
+        expect(t.root.right.key).to.equal(45);
+        expect(t.root.isRed()).to.be.false;
+        expect(t.root.left.isRed()).to.be.false;
+        expect(t.root.right.isRed()).to.be.false;
+        expect(t.isRBT()).to.be.true;
+        expect(t.root.isRBT()).to.equal(3);
+    });
+    it("have a deleteMin method", function() {
+        var N = 100, A = [];
+        for (var i = 0; i< N; i++) {
+            A.push(Math.random());
+            t.put(A[i]);
+        }
+        A.sort();
+        for (var i = 0; i< N; i++) {
+            expect(t.min()).to.equal(A[i]);
+            expect(t.isBST()).to.be.true;
+            expect(t.isRBT()).to.be.true;
+            expect(t.size()).to.equal(N-i);
+            t.deleteMin();
+        }
+    });
+    it("have a deleteMax method", function() {
+        var N = 100, A = [];
+        for (var i = 0; i < N; i++) {
+            A.push(Math.random());
+            t.put(A[i]);
+        }
+        A.sort().reverse();
+        for (var i = 0; i < N; i++) {
+            expect(t.max()).to.equal(A[i]);
+            expect(t.isRBT()).to.be.ok;
+            expect(t.size()).to.equal(N-i);
+            t.deleteMax();
+        }
+    });
+    it("have a delete method", function() {
+        var N = 100, A = [];
+        for (var i = 0; i< N; i++) {
+            A.push(Math.random());
+            t.put(A[i], "");
+        }
+        expect(t.size()).to.equal(A.length);
+        for (var i = N; i--;) {
+            var j = Math.floor(Math.random() * i),
+                temp = A[j];
+            A[j] = A[i];
+            A.pop();
+            expect(t.has(temp)).to.be.ok;
+            t.delete(temp);
+            expect(t.has(temp)).not.to.be.ok;
+            expect(t.isRBT()).to.be.ok;
+            expect(t.size()).to.equal(A.length);
+        }
     });
 });
