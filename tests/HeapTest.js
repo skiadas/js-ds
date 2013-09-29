dd28dee19b063ccf5eaa84c17b4d7e77be8bdb41
Haris Skiadas <cskiadas@gmail.com>
2013-09-29 16:20:18 -0400

    Tests for Heaps

--- tests/HeapTest.js
+++ tests/HeapTest.js
@@ -1,37 +1,96 @@
-Heap = require('../Heap.js');
-var A = new Heap("v");
+var chai = require('chai'),
+    expect = chai.expect,
+    Heap = require('../Heap.js');
+chai.Assertion.includeStack = true;
 
-// You can then read the components by calling A.components();
-// Or call A.components(i) to get the component containing i;
-
-// console.log(A.isLess({v: 12}, {v: 13}))
-// console.log(A.isLess({v: 13}, {v: 12}))
-var now = new Date();
-N = 1e6;
-for (var i=0; i<N; i++) {
-    var val = Math.floor(Math.random() * 30)
-    // console.log("Inserting: ", val);
-    A.insert({v: val});
-}
-console.log(N + " inserts in " + (new Date() - now) + "ms.");
-console.log(A.length);
-// A.insert({v: 5}).insert({v:2})
-now = new Date();
-var prev = A.extract();
-while (!A.isEmpty()) { 
-    var next = A.extract();
-    if (next < prev) { throw new Error("Danger Will Robinson!"); }
-    prev = next;
-}
-console.log(N + " extracts in " + (new Date() - now) + "ms.");
-
-
-for (var i=0; i<10; i++) {
-    var val = Math.floor(Math.random() * 30)
-    // console.log("Inserting: ", val);
-    A.insert({v: val});
-}
-
-while (!A.isEmpty()) {
-    console.log(A.extract());
-}
+describe("Heaps", function() {
+    var h;
+    beforeEach(function(){
+        h = new Heap("v");
+    });
+    describe("have supported operations: insert, extract, min", function() {
+        it("insert", function() {
+            var obj = { v: Math.random() };
+            expect(function() { h.insert(obj); }).to.not.throw;
+            h.insert(obj);
+            expect(h.size()).to.equal(1);
+            expect(h.min()).to.equal(obj);
+            expect(h.size()).to.equal(1);
+            expect(h.extract()).to.equal(obj);
+            expect(h.size()).to.equal(0);
+        });
+        it("have secondary operations: isEmpty, isHeap, size", function() {
+            var N = 1000, i;
+            for (i = 0; i < N; i++) {
+                expect(h.isHeap()).to.be.ok;
+                expect(h.isEmpty()).to.be.equal(i === 0);
+                expect(h.size()).to.equal(i);
+                h.insert({v: Math.random() });
+            }
+        });
+        it("have a deleteObj operation if initialized that way", function() {
+            var A = [], N = 1000, i;
+            for (i = 0; i < N; i++) { A.push({v: Math.random() }) };
+            h.insert(A[0]);
+            expect(function() { h.deleteObj(A[0]); }).to.throw;
+            h = new Heap("v", true);
+            for (i = 0; i < N; i++) { h.insert(A[i]); }
+            for (i = 0; i < N; i++) {
+                expect(h.size()).to.equal(N-i);
+                expect(h.isHeap()).to.be.ok;
+                expect(function() { h.deleteObj(A[i]); }).to.not.throw;
+                h.deleteObj(A[i]);
+            }
+        });
+    });
+    it("isHeap works correctly", function() {
+        var i, N = 100, K = N * 2, A = [], M = 10000;
+        for (i = 0; i < N; i++) {
+            A[i] = { v: i };
+            h.insert(A[i]);
+        }
+        expect(h.isHeap()).to.be.ok;
+        while (M--) {
+            i = Math.floor(Math.random() * (N-1) + 1);
+            // console.log(M, i);
+            A[i].v = A[i].v - K;
+            expect(h.isHeap()).to.not.be.ok;
+            A[i].v = A[i].v + K;
+            expect(h.isHeap()).to.be.ok;
+        }
+    });
+    it("min is always the smallest element", function() {
+        var i, N = 100, K = N * 2, obj;
+        for (i = 0; i < N; i++) { h.insert({ v: i }); }
+        for (i = 0; i < N; i++) {
+            expect(h.min().v).to.equal(i);
+            obj = h.extract();
+            expect(obj.v).to.equal(i);
+        }
+    });
+    it("delete works correctly for removing the last item", function() {
+        var i, N = 100, K = N * 2, obj;
+        for (i = 0; i < N; i++) { h.insert({ v: i }); }
+        for (i = N; i-- > 1;) {
+            expect(function() { h.delete(i + 1); }).to.throw;
+            expect(function() { h.delete(i); }).to.not.throw;
+            h.delete(i);
+            expect(h.isHeap()).to.be.ok;
+            expect(h.size()).to.equal(i);
+        }
+    });
+    describe("can work without an isLess comparator/key for simple objects (numbers)", function() {
+        beforeEach(function(){ h = new Heap(); });
+        it("can insert, extract, min", function() {
+            var A = [], i, N = 100;
+            for (i = 0; i < N; i++) {
+                A[i] = Math.random(); h.insert(A[i]);
+            }
+            A = A.sort();
+            for (i = 0; i < N; i++) {
+                expect(h.min()).to.equal(A[i]);
+                expect(h.extract()).to.equal(A[i]);
+            }
+        });
+    });
+});
