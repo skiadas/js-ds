761101acaa373978d48051ae092e1c423c4630fe
Haris Skiadas <cskiadas@gmail.com>
2013-10-01 00:55:26 -0400

    Rudimentary Set class, needs more tests

---
+++ Set.js
@@ -0,0 +1,89 @@
+// Set class
+// Assumes the elements are either strings or have a toString method.
+// Also the set's string representation assumes the element strings
+// don't contain curly braces or commas.
+
+(function(define){ 'use strict';
+define(function(require) {
+    
+    function Set(arr) {
+        this._elements = {};
+        var that = this;
+        if (arr != null && arr.length != null) {
+            arr.forEach(function(k) { that.add(k); });
+        }
+    }
+    
+    Set.prototype = {
+        add: function(k) {
+            this._elements[k.toString()] = k;
+            return this;
+        },
+        delete: function(k) {
+            k = k.toString();
+            var val = this._elements[k];
+            delete this._elements[k];
+            return val;
+        },
+        contains: function(k) {
+            return this._elements.hasOwnProperty(k.toString());
+        },
+        intersection: function(set) {
+            if (arguments.length > 1) {
+                return this.intersection.apply(
+                    this.intersection(set),
+                    [].slice.call(arguments, 1)
+                );
+            }
+            var ret = new Set(), els = this._elements;
+            for (var k in els) {
+                if (els.hasOwnProperty(k) && set.contains(k)) {
+                    ret.add(els[k]);
+                }
+            }
+            return ret;
+        },
+        union: function(set) {
+            if (arguments.length > 1) {
+                return this.union.apply(
+                    this.union(set),
+                    [].slice.call(arguments, 1)
+                );
+            }
+            var ret = set.clone(), els = this._elements;
+            for (var k in els) {
+                if (els.hasOwnProperty(k)) { ret.add(els[k]); }
+            }
+            return ret;
+        },
+        complement: function(universe) {
+            return universe.difference(this);
+        },
+        difference: function(set) {
+            var ret = new Set(), els = this._elements;
+            for (var k in els) {
+                if (els.hasOwnProperty(k) && !set.contains(k)) {
+                    ret.add(els[k]);
+                }
+            }
+            return ret;
+        },
+        without: function(key) {
+            return this.difference(new Set([key]));
+        },
+        elements: function() {
+            var arr = [], els = this._elements;
+            for (var k in els) {
+                if (els.hasOwnProperty(k)) { arr.push(els[k]); }
+            }
+            return arr;
+        },
+        toString: function() {
+            return '{' + this.elements().join(',') + '}';
+        },
+        clone: function() { return new Set(this.elements()); }
+    }
+    
+    return Set;
+});
+}(typeof define === 'function' ? define : function(factory) { module.exports = factory(require); }));
---
+++ tests/SetTest.js
@@ -0,0 +1,61 @@
+var chai = require('chai'),
+    expect = chai.expect,
+    Set = require('../Set.js'),
+    range = require('../utils/range.js');
+
+chai.Assertion.includeStack = true;
+
+describe("In Set objects", function() {
+    var s, t1, t2;
+    beforeEach(function() {
+        s = new Set();
+        t1 = Math.random(), t2 = Math.random();
+    })
+    it("can 'add' elements", function() {
+        expect(function() {s.add(23)}).to.not.throw;
+        expect(function() {s.add(46)}).to.not.throw;
+    });
+    it("can check if set 'contains' an element", function() {
+        s.add(t1).add(t1+t2);
+        expect(s.contains(t1)).to.be.ok;
+        expect(s.contains(t1+t2)).to.be.ok;
+        expect(s.contains(t1+t2+1)).to.not.be.ok;
+    });
+    it("can 'delete' elements", function() {
+        s.add(t1).add(t1+t2);
+        expect(s.delete(t1+t2)).to.equal(t1+t2);
+        expect(s.contains(t1+t2)).to.not.be.ok;
+        expect(s.contains(t1)).to.be.ok;
+    });
+    it("can retrieve an array of the 'elements'", function() {
+        s.add(t1).add(t1+t2);
+        var A = s.elements();
+        expect(A.length).to.equal(2);
+        expect(A).to.contain(t1);
+        expect(A).to.contain(t1+t2);
+    });
+    it("can form 'intersection' of two sets", function() {
+        s.add(t1).add(t1+t2);
+        var s2 = new Set();
+        s2.add(t1).add(t1+2*t2);
+        var s3 = s.intersection(s2);
+        expect(s3.elements().sort()).to.deep.equal([t1]);
+    });
+    it("can form 'union' of two sets", function() {
+        s.add(t1).add(t1+t2);
+        var s2 = new Set();
+        s2.add(t1).add(t1+2*t2);
+        var s3 = s.union(s2);
+        expect(s3.elements().sort()).to.deep.equal([t1, t1+t2, t1+2*t2]);
+    });
+    it("union and intersection can accept a variable list of arguments", function() {
+        s.add(t1).add(t1+t2);
+        var s2 = new Set(), s3 = new Set();
+        s2.add(t1).add(t1+2*t2);
+        s3.add(t1).add(t1+3*t2);
+        var s4 = s.union(s2, s3);
+        expect(s4.elements().sort()).to.deep.equal([t1, t1+t2, t1+2*t2, t1+3*t2]);
+        var s5 = s.intersection(s2, s3);
+        expect(s5.elements().sort()).to.deep.equal([t1]);
+    });
+});
